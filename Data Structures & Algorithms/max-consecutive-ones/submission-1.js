424d547ecffaf5d52361351cb9b55f502b2bf0ba
Brandon Ha <idevbrandon@gmail.com>
2026-04-06 22:53:05 -0400

    Add: max-consecutive-ones - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let count = 0;
+        let max = 0;
+        for(let i=0; i<nums.length; i++) {
+            if(nums[i] === 1) {
+                count++;
+                if (count > max) max = count;
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
